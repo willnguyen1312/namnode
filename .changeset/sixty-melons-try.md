57464569647c2d6e5bd643f1dbb3187e483c71ea
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-05-28 09:27:42 -0300

    Add test coverage 💞 (#1)

---
+++ .changeset/sixty-melons-try.md
@@ -0,0 +1,6 @@
+---
+"@namnode/store": minor
+"@namnode/utils": minor
+---
+
+Add test coverage 💞
---
+++ packages/utils/test/utils.test.ts
@@ -0,0 +1,54 @@
+import fc from "fast-check"
+import { faker } from "@faker-js/faker"
+import { clamp, makeMaybeCallFunction } from "../src"
+import { it } from "vitest"
+
+describe("clamp functions", () => {
+  it("should work for value in between inclusively", () => {
+    fc.assert(
+      fc.property(fc.integer(), fc.integer(), (first, second) => {
+        const [min, max] = [Math.min(first, second), Math.max(first, second)]
+        const value = faker.number.int({ min, max })
+        expect(clamp(value, min, max)).toBe(value)
+      }),
+    )
+  })
+
+  it("should work for value under bound", () => {
+    fc.assert(
+      fc.property(fc.integer(), fc.integer(), (first, second) => {
+        const [min, max] = [Math.min(first, second), Math.max(first, second)]
+        const value = faker.number.int({
+          min: Number.MIN_SAFE_INTEGER,
+          max: min - 1,
+        })
+        expect(clamp(value, min, max)).toBe(min)
+      }),
+    )
+  })
+
+  it("should work for value upper bound", () => {
+    fc.assert(
+      fc.property(fc.integer(), fc.integer(), (first, second) => {
+        const [min, max] = [Math.min(first, second), Math.max(first, second)]
+        const value = faker.number.int({
+          min: max + 1,
+          max: Number.MAX_SAFE_INTEGER,
+        })
+        expect(clamp(value, min, max)).toBe(max)
+      }),
+    )
+  })
+})
+
+describe("makeMaybeCallFunction function", () => {
+  it("should call function based on predicate return value", () => {
+    fc.assert(
+      fc.property(fc.boolean(), (value) => {
+        const mockFn = vi.fn()
+        makeMaybeCallFunction(() => value, mockFn)(void 0)
+        expect(mockFn).toHaveBeenCalledTimes(value ? 1 : 0)
+      }),
+    )
+  })
+})
