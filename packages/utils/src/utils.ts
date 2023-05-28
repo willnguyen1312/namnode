b9da1bc5eea055f2ecea71c093c9fdd7d11cd7aa
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-05-28 08:43:55 -0300

    🚀

--- packages/utils/src/utils.ts
+++ packages/utils/src/utils.ts
@@ -4,3 +4,11 @@ export function clamp(value: number, min: number, max: number) {
 
 // Credit: https://github.com/kentor/flush-promises
 export const flushPromises = () => new Promise((resolve) => setTimeout(resolve))
+
+export function makeMaybeCallFunction<T>(predicateFn: () => boolean, fn: (arg: T) => void) {
+  return (arg: T) => {
+    if (predicateFn()) {
+      fn(arg)
+    }
+  }
+}
---
+++ packages/utils/vite.config.ts
@@ -0,0 +1,11 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  plugins: [],
+  test: {
+    globals: true,
+    environment: "happy-dom",
+  },
+})
