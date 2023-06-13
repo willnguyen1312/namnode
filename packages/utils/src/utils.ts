a365189e13a4a5d17fc54bcf9032d2af598cad71
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-06-13 11:04:28 -0300

    Add loadScript util 🚀

--- packages/utils/src/utils.ts
+++ packages/utils/src/utils.ts
@@ -14,3 +14,33 @@ export function makeMaybeCallFunction<T>(predicateFn: () => boolean, fn: (arg: T
 }
 
 export const sleep = (millisecond: number) => new Promise((resolve) => setTimeout(resolve, millisecond))
+
+const createLoadScript = () => {
+  const scriptSet = new Set<string>()
+
+  async function loadScript(url: string) {
+    if (scriptSet.has(url)) {
+      return Promise.resolve()
+    }
+
+    return new Promise<void>(function (resolve, reject) {
+      const scriptElement = document.createElement("script")
+      scriptElement.src = url
+
+      scriptElement.onload = function () {
+        scriptSet.add(url)
+        resolve()
+      }
+
+      scriptElement.onerror = function () {
+        reject(new Error(`Failed to load script: ${url}`))
+      }
+
+      document.head.appendChild(scriptElement)
+    })
+  }
+
+  return loadScript
+}
+
+export const loadScript = createLoadScript()
