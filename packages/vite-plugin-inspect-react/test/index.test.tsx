e91f93290d3fe885c47f81cceb2583950b9b3e24
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-01-07 17:53:50 -0400

    Improve test case 💞

--- packages/vite-plugin-inspect-react/test/index.test.tsx
+++ packages/vite-plugin-inspect-react/test/index.test.tsx
@@ -1,25 +1,50 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { describe, expect, it } from "vitest"
 import { App } from "../playground/App"
 
+function traverseBottomUp(node: HTMLElement, predicate: (node: HTMLElement) => HTMLElement | null) {
+  let currentNode: Node | null = node
+  while (currentNode) {
+    const found = predicate(currentNode as HTMLElement)
+    if (found) {
+      return found
+    }
+    currentNode = currentNode.parentElement
+  }
+
+  return null
+}
+
 describe("vite-plugin-inspect-react", () => {
   it("should work", () => {
-    const { container } = render(<App />)
+    render(<App />)
 
-    expect(container).toMatchInlineSnapshot(`
-  <div>
-    <span
-      hidden=""
-      id="/Users/namnguyen/vn/github/namnode/packages/vite-plugin-inspect-react/test/index.test.tsx:7:33"
-    />
-    <span
-      hidden=""
-      id="/Users/namnguyen/vn/github/namnode/packages/vite-plugin-inspect-react/playground/App.tsx:6:6"
-    />
-    <h1>
-      Hi there 💞
-    </h1>
-  </div>
-`)
+    const hiThereText = screen.getByText(/hi there/i)
+
+    const nearestInjectedNode = traverseBottomUp(hiThereText, (node) => {
+      // const isCurrentNode
+      // return !!node.hidden && node.tagName === "SPAN"
+
+      let currentNode = node
+
+      while (currentNode.previousSibling) {
+        const sib = currentNode.previousSibling as HTMLElement
+
+        if (sib.hasAttribute("hidden") && sib.tagName === "SPAN") {
+          return sib
+        }
+
+        currentNode = sib
+      }
+
+      return null
+    })
+
+    if (!nearestInjectedNode) {
+      throw new Error("No injected node found")
+    }
+
+    const h1Position = `playground/App.tsx:6:6`
+    expect(nearestInjectedNode.id).toContain(h1Position)
   })
 })
